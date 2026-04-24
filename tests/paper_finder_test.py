d98113650096a578927e3726272631d46d5b631f
Cash <cashhilstad@gmail.com>
2026-04-23 21:55:37 -0700

    test: added tests for keyword creation

--- tests/paper_finder_test.py
+++ tests/paper_finder_test.py
@@ -1,6 +1,30 @@
 from library.paper_finder.paper_finder import PaperFinder
 from library.search_input.search_input import SearchInput
 
+def test_keyword_creation_a() -> None:
+    pf = PaperFinder()
+    search = SearchInput(query="Hello, World!")
+    success = pf.get_seach_keywords(search) == ["Hello", "World"]
+    assert success == True
+
+def test_keyword_creation_b() -> None:
+    pf = PaperFinder()
+    search = SearchInput(query="abcdef")
+    success = pf.get_seach_keywords(search) == ["abcdef"]
+    assert success == True
+
+def test_keyword_creation_c() -> None:
+    pf = PaperFinder()
+    search = SearchInput(query="123.456.789")
+    success = pf.get_seach_keywords(search) == ["123456789"]
+    assert success == True
+
+def test_keyword_creation_d() -> None:
+    pf = PaperFinder()
+    search = SearchInput(query="!@#$%^&*")
+    success = pf.get_seach_keywords(search) == [""]
+    assert success == True
+
 def test_url_creation_no_keyword() -> None:
     pf = PaperFinder()
     search = SearchInput(query="")

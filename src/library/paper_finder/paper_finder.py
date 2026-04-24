43c4485935c4e9224ac5abad0e0f9060ae5f870b
Cash <cashhilstad@gmail.com>
2026-04-23 21:47:53 -0700

    refactor: moved input to src subfolder, integrate
    integrated SearchInput into paper_finder

--- src/library/paper_finder/paper_finder.py
+++ src/library/paper_finder/paper_finder.py
@@ -1,4 +1,6 @@
 from library.webscraper.webscraper import Webscraper
+from library.search_input.search_input import SearchInput
+import string
 
 SCHOLAR_URL = "https://scholar.google.com/scholar?q="
 
@@ -6,8 +8,17 @@ class PaperFinder:
     def __init__(self):
         self.webscraper = Webscraper()
 
-    def create_url(self, keywords : list) -> str:
+    ## Strips keywords of special characters for searching.
+    def get_seach_keywords(self, search : SearchInput) -> list:
+        clean_query : str = search.query.translate(str.maketrans('', '', string.punctuation))
+        keywords = clean_query.split(" ")
+        return keywords
+
+    ## Creates a url to scrape based on the search data.
+    def create_url(self, search : SearchInput) -> str:
         target_url : str = SCHOLAR_URL
+
+        keywords = self.get_seach_keywords(search)
         keyword_count : int = len(keywords)
         for index, keyword in enumerate(keywords):
             target_url += keyword
@@ -16,7 +27,7 @@ class PaperFinder:
 
         return target_url
 
-    def search_by_keywords(self, keywords : list) -> dict:
-        target_url = self.create_url(keywords)
+    def search(self, search : SearchInput) -> dict:
+        target_url = self.create_url(search)
         self.webscraper.load_url()
         pass
--- tests/paper_finder_test.py
+++ tests/paper_finder_test.py
@@ -1,21 +1,26 @@
 from library.paper_finder.paper_finder import PaperFinder
+from library.search_input.search_input import SearchInput
 
 def test_url_creation_no_keyword() -> None:
     pf = PaperFinder()
-    success = pf.create_url([]) == "https://scholar.google.com/scholar?q="
+    search = SearchInput(query="")
+    success = pf.create_url(search) == "https://scholar.google.com/scholar?q="
     assert success == True
 
 def test_url_creation_one_keyword() -> None:
     pf = PaperFinder()
-    success = pf.create_url(["hello"]) == "https://scholar.google.com/scholar?q=hello"
+    search = SearchInput(query="hello")
+    success = pf.create_url(search) == "https://scholar.google.com/scholar?q=hello"
     assert success == True
 
 def test_url_creation_two_keywords() -> None:
     pf = PaperFinder()
-    success = pf.create_url(["hello", "world"]) == "https://scholar.google.com/scholar?q=hello+world"
+    search = SearchInput(query="hello world")
+    success = pf.create_url(search) == "https://scholar.google.com/scholar?q=hello+world"
     assert success == True
 
 def test_url_creation_three_keywords() -> None:
     pf = PaperFinder()
-    success = pf.create_url(["this", "is", "a"]) == "https://scholar.google.com/scholar?q=this+is+a"
+    search = SearchInput(query="this is a")
+    success = pf.create_url(search) == "https://scholar.google.com/scholar?q=this+is+a"
     assert success == True

d6cb7e19fb66921c92c2a68a78d6bf043c76f7e3
Cash <cashhilstad@gmail.com>
2026-04-25 17:01:02 -0700

    feat: full search function
    PaperFinder can now fully search and get paper results.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
+from library.paper_finder.paper_finder import PaperFinder
+from library.search_input.search_input import *
+
 def main():
     print("Hello from cpsc334-finalp searching branch!")
+    p = PaperFinder()
+    search_input = parse_cli_args()
+    r = p.search(search_input)
+    print(r)
 
 if __name__ == "__main__":
     main()
